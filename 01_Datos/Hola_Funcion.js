784af72ae5221a4a15c0f46f1c5c656307519079
indian705 <indian705@hotmail.com>
2018-06-21 13:58:26 +0200

    01_Datos

---
+++ 01_Datos/Hola_Funcion.js
@@ -0,0 +1,20 @@
+/*
+  JavaScript (@Oracle) -> ECMAScript -> ES6 / ES2015
+ */
+
+var resultado
+
+ function saludar(pUser) {
+    const SALUDO = 'Hola'
+    console.log(SALUDO, pUser)
+}
+
+function sumar(a,b){
+    let resultado = a+b
+    console.log(resultado)
+}
+
+saludar('Pepe')
+saludar('Elena')
+sumar(3,6)
+
---
+++ 01_Datos/Hola_Mundo.js
@@ -0,0 +1,17 @@
+/** Declaracion */
+var saludo
+/** Inicializacion */
+saludo = 'Hola'
+/*
+Tipos de datos
+- string
+- number
+- booleans (true / false)
+- object
+- undefined
+*/
+
+/** Declaracion + Inicializacion */
+var user = 'Mundo'
+
+console.log(saludo, user)
