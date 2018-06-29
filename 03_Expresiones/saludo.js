14a15ceab57c5d1bf1ec86d100b90b98440e1ae4
indian705 <indian705@hotmail.com>
2018-06-29 12:13:52 +0200

    03_Expresiones

---
+++ 03_Expresiones/saludo.js
@@ -0,0 +1,53 @@
+/* function saludar (edad){
+    let sal
+    if (edad < 12) {
+        sal='Hola chaval'
+    }
+    else if (edad < 30) {
+        sal='Hola colega'
+    }
+    else if (edad < 70) {
+        sal='Buenos días'
+    }
+    else {
+        sal='Saludos, maestro'
+    }
+    return sal       
+} */
+
+function saludar (edad) {
+    let aMensajes = [
+        'Hola chaval', 
+        'Hola colega', 
+        'Buenos dias', 
+        'Saludos, maestro'
+    ]
+    let mensaje = 3
+    if ( edad < 18) {
+        mensaje = 0
+    } else if ( edad  <30 ) {
+        mensaje = 1
+    } else if ( edad  <70 ) {
+        mensaje = 2
+    }
+    return aMensajes[mensaje];
+}
+
+/* let edad=8
+let mensaje = `Si tu edad es ${edad}, ${saludar(edad)}`
+console.log(mensaje) */
+
+
+
+function probar () {
+    let edad = 10
+    console.log(`Si tu edad es ${edad}`, saludar(edad)) // Hola chaval
+    edad = 19
+    console.log(`Si tu edad es ${edad}`, saludar(edad)) // Hola colega
+    edad = 40
+    console.log(`Si tu edad es ${edad}`, saludar(edad)) // Buenos dias
+    edad = 80
+    console.log(`Si tu edad es ${edad}`, saludar(edad)) // Saludos, maestro
+}
+
+probar()
