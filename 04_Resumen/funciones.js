ec116b72494927c776574200eb88a208a3c583dc
indian705 <indian705@hotmail.com>
2018-07-09 12:08:29 +0200

    04_Resumen

---
+++ 04_Resumen/funciones.js
@@ -0,0 +1,57 @@
+//Declaración
+
+
+// Calcular el área de diversas figuras
+
+/**
+ * 
+ * Calcular area de diversas figuras
+ * 
+ * @param:string nombre de la figura
+ * @param:object  datos de la figura
+ *  si es triangulo h y b
+ *  si es cuadrado l
+ *  si es circulo r
+ * @return:number   area de la figura
+ * 
+ */
+
+function calcularArea(figura,datos){
+    let resultado = 0
+    switch (figura.toLowerCase()) {
+        case 'triangulo':
+        case 'triángulo':
+            resultado = (datos.h * datos.b) / 2           
+            break;
+        case 'cuadrado':
+            resultado = datos.l * datos.l
+            break;
+        case 'circulo':
+        case 'círculo':
+            resultado = Math.PI * datos.r * datos.r
+            break;
+        default:
+            break;
+    }
+    return resultado.toFixed(2)
+
+}
+
+//Uso o invocación
+
+function probar(){
+    let sFigura = 'triángulO'
+    let oDatos = {h:12, b:10}
+    
+    console.log(calcularArea(sFigura,oDatos))
+    sFigura = 'Cuadrado'
+    oDatos =  {l:10}
+    console.log(calcularArea(sFigura,oDatos))
+
+    sFigura = 'Círculo'
+    oDatos =  {r:5}
+    console.log(calcularArea(sFigura,oDatos))
+
+}
+
+probar()
