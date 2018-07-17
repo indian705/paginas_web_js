4badf163422448f545a6bf631ca16e03b93e6ede
indian705 <indian705@hotmail.com>
2018-07-17 12:31:57 +0200

    06_Web

--- 06_Web/Hola_amigo/app.js
+++ 06_Web/Hola_amigo/app.js
@@ -1,15 +1,29 @@
 function app () {
 
-    domNombre = document.querySelector("#nombre")
-    domBtnSaludar = document.querySelector("#btnSaludar")
-    domBtnBorrar = document.querySelector("#btnBorrar")
+    //Defino los nodos del DOM que me interesan
+    let domNombre = document.querySelector("#nombre")
+    let domBtnSaludar = document.querySelector("#btnSaludar")
+    let domBtnBorrar = document.querySelector("#btnBorrar")
+    let domOutput = document.querySelector("#output")
 
-    domNombre.addEventListener('input',leerDatos)
+    //Defino los manejadores de eventos de algunos nodos
+    domNombre.addEventListener('input', leerDatos)
+    domNombre.addEventListener('change', detectarChange)
     domBtnSaludar.addEventListener('click', saludar)
     domBtnBorrar.addEventListener('click', borrar)
-    
-    function leerDatos(){
-        console.log(domNombre.value)
+        
+    function leerDatos() {
+        let valorNombre = domNombre.value
+        console.log('Input detectado')
+        console.log('valorNombre: ', valorNombre)
+        domOutput.innerHTML = `Hola, <strong>${valorNombre}</strong>`
+    }
+
+
+    function detectarChange(){
+        let finalNombre = domNombre.value
+        console.log('Change detectado')
+        console.log('finalNombre: ', finalNombre)
     }
 
     function saludar(){
---
+++ 06_Web/Hola_amigo/app_new.js
@@ -0,0 +1,11 @@
+
+function app() {
+
+    // Definir los nodos del DOM
+    let = document.querySelector('#nombre')
+
+    // Definir manejadores de eventos
+}
+
+document.addEventListener('DOMContentLoaded', app)
+
