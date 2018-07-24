d865943b4fc9158262f482c7b6469176737517cf
indian705 <indian705@hotmail.com>
2018-07-24 13:55:33 +0200

    08_Fechas

---
+++ 08_Fechas/fechas.js
@@ -0,0 +1,11 @@
+let fecha = new Date(1945,10,4)
+fecha = new Date(1954475347853)
+console.log(fecha)
+console.log(fecha.toString())
+console.log(fecha.toDateString())
+console.log(fecha.toISOString())
+console.log(fecha.toGMTString())
+
+console.log(fecha.getFullYear())
+console.log(fecha.getDate())
+console.log(new Date().getDay())
