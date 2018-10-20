a0643d066ca312b2a665341117f020e6f18620e1
indian705 <indian705@hotmail.com>
2018-10-21 01:13:03 +0200

    proyecto

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,14 +1,15 @@
 class App {
 
     constructor () {
-        /* this.btnTop = document.querySelector('#btn-top')
+        this.btnTop = document.querySelector('#btn-top')
         this.divTop = document.querySelector('#div-top')
         
-        this.parrafoHeader = document.querySelector('header p')
-        */
+        //this.parrafoHeader = document.querySelector('header p')
+        
 
-       this.aNodosMenu = document.querySelectorAll('.menu-desk a')
-       //this.aNodosMenu = document.querySelectorAll('.menu-desk a:not(:last-child)')
+       //this.aNodosMenu = document.querySelectorAll('.menu-desk a')
+       //this.navHeight = document.querySelector('.menu-desk').offsetHeight
+       this.aNodosMenu = document.querySelectorAll('.menu-desk a:not(:last-child)')
        this.aNodosSection = document.querySelectorAll('main section')
 
         this.header = document.querySelector('header')
@@ -16,11 +17,16 @@ class App {
         this.nodoh1 = document.querySelector('h1')
 
         
-        /* this.btnTop.addEventListener('click', this.subir.bind(this))  */
+        this.btnTop.addEventListener('click', this.subir.bind(this)) 
 
         this.menuHamburguesa = document.querySelector('#menu-hamburguesa')
         this.contenidoHamburguesa = document.querySelector('#contenido-hamburguesa')
         this.itemHamburguesa = document.querySelectorAll('#contenido-hamburguesa a')
+
+        
+        this.slideInterval = 3000
+        this.aFigure = document.querySelectorAll('figure')
+        this.empezarCarrousel()
 
         this.aOffset = []
         this.calcularOffsets()
@@ -86,11 +92,11 @@ class App {
             this.bajoHeader.classList.remove('bajo-header')
             this.bajoHeader.classList.add('hide')
         }
-        /* if (oE.target.scrollingElement.scrollTop > 100) {
+        if (position > 100) {
             this.divTop.classList.remove('hide')
         } else {
             this.divTop.classList.add('hide')
-        } */
+        }
     }
 
     navegar(oE) {
@@ -110,8 +116,8 @@ class App {
         this.aNodosSection.forEach(
             (section) => {
                 //console.dir(section.offsetParent) para comprobar que todas las secciones como ancestro el body de cara a offset
-                this.aOffset.push(section.offsetTop)
-                //this.aOffset.push(section.offsetTop-200)
+                //this.aOffset.push(section.offsetTop-this.navHeight)
+                this.aOffset.push(section.offsetTop-70)
             }
         )
         this.aOffset[0] = 0
@@ -125,6 +131,28 @@ class App {
             left: 0, 
             behavior: 'smooth'
         })
+    }
+
+    avanzar(){
+        let pointer = 0;
+        this.aFigure.forEach(
+            (item, i)=>{
+                if (item.className=='visible') {
+                    item.className=''
+                    pointer = i
+                }
+            }
+        )
+        if (++pointer==this.aFigure.length){
+            pointer = 0
+        }
+        this.aFigure[pointer].className='visible'
+        
+        setTimeout(this.avanzar.bind(this), this.slideInterval)
+    }
+
+    empezarCarrousel() {
+        setTimeout(this.avanzar.bind(this), this.slideInterval);
     }
 }
 
